f5d5f0f4b0b03e55b76692934facd5e5950962d0
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-07 07:47:24 -0800

    Update about.lua

--- plugins/about.lua
+++ plugins/about.lua
@@ -1,13 +1,13 @@
 do
     local function run(msg, matches)
-        return " X-ViRuS-X V 1\nAdvanced Bot Base On Seed\n\n@VvViruSs[DeVeLoPeR]\n\n#Open_Source_On [@WilSoN_DeVeLoPeR]"
+        return " DEAD_KINGs V 1\nAdvanced Bot Base On Seed\n\n@KinG0fDeaD[DeVeLoPeR]\n\n#Open_Source_On [@DeaD_t34M]"
     end
     return {
         usage = {
-            "virus: About Current Bot.",
+            "dead: About Current Bot.",
             },
         patterns = {
-            "^([Vv]irus)$",
+            "^([Dd]ead)$",
             },
         run = run 
     }
