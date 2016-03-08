6d19a4c6fa68cbae55f402233a2ab439a1e909a7
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-08 03:25:53 -0800

    Update about.lua

--- plugins/about.lua
+++ plugins/about.lua
@@ -1,6 +1,6 @@
 do
     local function run(msg, matches)
-        return " DEAD_KINGs V 1\nAdvanced Bot Base On Seed\n\n@KinG0fDeaD[DeVeLoPeR]\n\n#Open_Source_On [@DeaD_t34M]\n\n#WE ARE With you[@KinGofFreaK]"
+        return " DEAD_KINGs V 1\nAdvanced Bot Base On Seed\n\n@KinG0fDeaD[DeVeLoPeR]\n\n@danyyyx\n#Open_Source_On [@DeaD_t34M]\n\n#WE_ARE_With_you[@KinGofFreaK]"
     end
     return {
         usage = {
